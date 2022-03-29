5cab0f4362e6b397e7a333383eb4d96a55cc1707
PaoloSalvitore <paolo.schaubert@gmail.com>
2022-03-30 08:48:47 +1100

    123

---
+++ Assets - Copy/Scripts/PlayerManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlayerManager : BaseManager
+{
+    private AIManager _aiManager;
+    [SerializeField] protected CanvasGroup _buttonGroup;
+    protected override void Start()
+    {
+        base.Start();
+        _aiManager = GetComponent<AIManager>(); //Checks to see if AIManager is attached
+        if (_aiManager == null)
+        {
+            Debug.LogError("AIManager not found");
+        }
+
+        if (_buttonGroup == null)
+        {
+            Debug.LogError("CanvasGroup _buttonGroup not attached"); //Checks to see if _buttonGroup is attached
+        }
+        
+    }
+
+    public override void TakeTurn()
+    {
+        _buttonGroup.interactable = true; //give the ability to click on a button
+    }
+
+    protected override void EndTurn()
+    {
+        _buttonGroup.interactable = false; //remvoes the ability to click on the button
+        _aiManager.TakeTurn();
+    }
+
+    public void Splash()
+    {
+        _aiManager.DealDamage(40.3f);
+        EndTurn();
+    }
+
+    public void IronTail()
+    {
+        _aiManager.DealDamage(10f); //deals 10 damage
+        EndTurn();
+    }
+
+    public void Rest()
+    {
+        Heal(30f); //heals player health by 50 and ends the turn
+        EndTurn();
+    }
+
+    public void SelfDestruct()
+    {
+        DealDamage(_maxHealth);
+        _aiManager.DealDamage(80f);
+        EndTurn();
+    }
+}
